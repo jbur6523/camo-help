08d2b91a4272cb3735e9c7f973f986f33ec3731c
jbur6523 <jbur6523@users.noreply.github.com>
2026-08-16 09:53:47 -0700

    Make preview middleware deployable

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import type { NextRequest } from "next/server";
-import { refreshSupabaseSession } from "@/lib/supabase/middleware";
+import { refreshSupabaseSession } from "./lib/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
   return refreshSupabaseSession(request);
